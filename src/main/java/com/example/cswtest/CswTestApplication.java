b3b252276d97270809301356378062637c340527
Huy Đào <huydqth2009022@fpt.edu.vn>
2022-08-18 09:53:17 +0700

    first commit

---
+++ src/main/java/com/example/cswtest/CswTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cswtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CswTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CswTestApplication.class, args);
+    }
+
+}
